cf8c27d6acef2c6e9d3a44b45b5618845a0c7e0e
haessae0 <haessae0@gmail.com>
2023-05-10 22:14:17 +0900

    치즈 프로젝트

---
+++ src/main/java/com/cheeze/cheezePlanner/CheezePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.cheeze.cheezePlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheezePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheezePlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheeze/cheezePlanner/CheezePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheeze.cheezePlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheezePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
